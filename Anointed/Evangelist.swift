27a77ae147d7e8793ff28756f001e544e4b547e0
Jacob Jackson <thinkmacinnovations@gmail.com>
2015-11-05 08:36:39 -0600

    Added office calling files

---
+++ Anointed/Evangelist.swift
@@ -0,0 +1,9 @@
+//
+//  Evangelist.swift
+//  Anointed
+//
+//  Created by Jacob Jackson on 11/5/15.
+//  Copyright © 2015 ThinkMac Innovations. All rights reserved.
+//
+
+import Foundation
---
+++ Anointed/Pastor.swift
@@ -0,0 +1,9 @@
+//
+//  Pastor.swift
+//  Anointed
+//
+//  Created by Jacob Jackson on 11/5/15.
+//  Copyright © 2015 ThinkMac Innovations. All rights reserved.
+//
+
+import Foundation
